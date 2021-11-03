332a5e2c6a2681ee94221901db8d02d8504de4a9
bruno <bruno.lucciano@gmail.com>
2021-11-02 23:28:23 -0300

    Inicial Game / Creation complete

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from time import sleep
 from rpg_construction import human
 from rpg_construction import monster
-from combat import combat
-from char_creation import character_creation
 
 def main():
+    print('Hi and welcome to my game!'
+          '\nThis is a pretty iniciant coder rpg game')
+    print()
+    name = input('So... tell me how do you wanna be called: ')
+
+    #Character creation
+    firstChar = human(name)
+
     #Monster creation
     monster_noob = monster()
     monster.noob(monster_noob)
@@ -18,42 +24,43 @@ def main():
     monster_boss = monster()
     monster.boss(monster_boss)
 
-
     #Combat definition
-    def combat_noob(human, monster):
+    def combat(human, monster):
         while firstChar.life > 0 or monster.life > 0:
-            monster.life - human.damage
+            monster.life - firstChar.damage
             print(f'Hit: {firstChar.damage}'
                   f'\nMonster HP: {monster.life}')
             if monster.life > 0:
-                damage_taken = human.life - monster.damage
-                human.life - monster.damage
-                print(f'Taken: {damage_taken}'
-                      f'\nHP: {human.life}')
+                firstChar.life - monster.damage
+                print(f'Taken: {monster.damage}'
+                      f'\nHP: {firstChar.life}')
 
-    #Game start!
-    #Monsters
-    monster_noob = monster()
-    monster.noob(monster_noob)
-    monster_medium = monster()
-    monster.medium(monster_medium)
-    monster_advanced = monster()
-    monster.advanced(monster_advanced)
-    monster_boss = monster()
-    monster.boss(monster_boss)
+    job = input('You can choose a job:'
+                           '\nMage (m/M)'
+                           '\nArcher (a/A)'
+                           '\nWarrior (w/W)')
+
+    if job == 'm' or job == 'M':
+        human.mage(firstChar)
+        print('Congrats! You are a mage now!')
+        firstChar.attribute_print()
+
+    if job == 'a' or job == 'A':
+        human.archer(firstChar)
+        print('Congrats! You are an archer now!')
+        print()
+        firstChar.attribute_print()
+
+    if job == 'g' or job == 'G':
+        human.warrior(firstChar)
+        print('Congrats! You are a warrior now!')
+        firstChar.attribute_print()
 
     #Introduction/Creation
-    print('Hi and welcome to my game!'
-          '\nThis is a pretty iniciant coder rpg game')
 
-    character_creation()
-
-    print()
     print('When your XP gets to 0, you will level up!')
     print('Now lets try a fight!')
     print()
-    combat(firstChar, monster_noob)
-
 
 if '__name__' == '__main__':
     main()
