6ad2b3614ee8066ca05d73cd4dbf74c957eb8b82
bruno <bruno.lucciano@gmail.com>
2021-11-02 22:49:13 -0300

    adding comba/updating character creation

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
 from time import sleep
 from rpg_construction import human
 from rpg_construction import monster
+from combat import combat
+from char_creation import character_creation
 
 def main():
 
     #Game start!
+    #Monsters
+    monster_noob = monster()
+    monster.noob(monster_noob)
+    monster_medium = monster()
+    monster.medium(monster_medium)
+    monster_advanced = monster()
+    monster.advanced(monster_advanced)
+    monster_boss = monster()
+    monster.boss(monster_boss)
+
+    #Introduction/Creation
     print('Hi and welcome to my game!'
-          '\nThis is a pretty iniciant coder rpg game'
-          '\n')
-    name = input('So express how you want to be called: ')
-    print()
-    print(f'Okay {name}, pleased to meet you!')
-    firstChar = human(name)
-    print()
-    print('In this game default attributes are:'
-          '\nLife: 10'
-          '\nStrength: 1'
-          '\nIntelligence: 1'
-          '\nDexterity: 1'
-          '\nLuck: random from 0 to 1')
-    print('-' * 30)
-    sleep(3)
+          '\nThis is a pretty iniciant coder rpg game')
 
-    print('In this game there are 3 main classes: Warrior, Archer and Mage.'
-          '\nEach class boosts attributes in a unique way'
-          '\nWarriors get more HP when level up, such as Archers get more Dex and Mages get Int')
-    job = input('Choose your destiny: Warrior (w/W)'
-                '\nArcher (a/A)'
-                '\nMage (m/M)'
-                '\nPress the correspondent key to select a class: ')
+    character_creation()
+
     print()
-    if job == 'w' or job == 'W':
-        human.warrior(firstChar)
-        print('Okay, you are a warrior now'
-              '\nEnjoy your starting attributes!')
-        firstChar.attribute_print()
-
-    if job == 'a' or job == 'A':
-        human.archer(firstChar)
-        print('Okay, you are an archer now'
-              '\nEnjoy your starting attributes!')
-        firstChar.attribute_print()
-
-    if job == 'm' or job == 'M':
-        human.mage(firstChar)
-        print('Okay, you are a mage now'
-              '\nEnjoy your starting attributes!')
-        firstChar.attribute_print()
+    print('When your XP gets to 0, you will level up!')
+    print('Now lets try a fight!')
+    print()
+    combat(firstChar, monster_noob)
 
 
 if '__name__' == '__main__':
--- rpg_construction.py
+++ rpg_construction.py
@@ -11,6 +11,7 @@ class human:
         self.xp = self.level * 20
         self.name = name
 
+
     def attribute_print(self):
         print(f'Life: {self.life}'
         f'\nStrenght: {self.stre}'
@@ -21,8 +22,6 @@ class human:
         print(f'Level: {self.level}'
         f'\nXP: {self.xp}')
 
-
-
 #lets say that stre, intel and dex are main stats, and life its essential but secondary
 #sooo 1 point of essential stats = 2.5 more importance at general effect
 #so 2 essential stats = 5 life (in weight)
@@ -32,18 +31,21 @@ class human:
         self.stre += 3
         self.intel += 1
         self.dex += 2
+        self.damage = self.stre * 1.5
 
     def archer(self): # +1 stre, +1 intel, +4 dex, +5 life = 1 + 1 + 4 + 2 = 8
         self.life += 5
         self.stre += 1
         self.intel += 1
         self.dex += 4
+        self.damage = self.dex * 1.5
 
     def mage(self): # +0 stre, + 5 intel, + 1 dex, + 5 life = 0 + 5 + 1 + 2 = 8
         self.life += 5
         self.stre += 0
         self.intel += 5
         self.dex += 1
+        self.damage = self.intel * 1.5
 
     def xp_gain(self, monster_xp):
         self.xp -= monster_xp
