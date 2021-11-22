da3659abaf6fd18bfc7fffcb0a047dbeac32b30e
bruno <bruno.lucciano@gmail.com>
2021-11-22 19:54:20 -0300

    mage class now can combat with nice chaos/harmonic spells

--- jogo_rpg_v02/combat.py
+++ jogo_rpg_v02/combat.py
@@ -11,18 +11,20 @@ def combat(character, monster):
                     pass
                 elif status == 's' or status == 'S':
                     print(f'===== Monster ====='
-                          f'\nHP: {monster.max_life}/{monster.life}'
+                          f'\nHP: {monster.life}/{monster.max_life}'
                           f'\n===== Player ====='
                           f'\nHP: {character.life}')
                 else:
-                    status = input('Sorry, your typing did not matched the options, try again!')
+                    status = input('Sorry, your typing did not matched the options, try again!'
+                                   '\nCombat: [c/C]'
+                                   '\nStatus: [s/S]')
                 print()
             damage = character.skill_attack()
             monster.life -= damage
             print()
             print(f'===== Monster ====='
                   f'\nSkill damage: {damage}'
-                  f'\nMonster HP: {monster.max_life}/{monster.life}'
+                  f'\nMonster HP: {monster.life}/{monster.max_life}'
                   f'\n===================')
             if monster.life > 0:
                 damage = monster.damage
@@ -30,7 +32,7 @@ def combat(character, monster):
                 print()
                 print(f'===== Player ====='
                       f'\nMonster damage: {damage}'
-                      f'\n{character.name} HP: {character.max_life}/{character.life}'
+                      f'\n{character.name} HP: {character.life}/{character.max_life}'
                       f'\n==================')
             else:
                 print()
@@ -40,15 +42,66 @@ def combat(character, monster):
                 character.xp -= monster.xp
                 if character.xp <= 0:
                     character.level_up()
-    elif character.job == 'mage':
+    elif character.job == 'mage': #I really need to create some functions for 'battle_choice' or something
         while character.life > 0 and monster.life > 0:
             status = input('Combat: [c/C]'
                            '\nStatus: [s/S]')
             if status == 'c' or status == 'C':
-                pass
+                combat_type = input('Chaos Magic: [cm/CM]'
+                                    '\nHarmonic Magic: [hm/HM]')
+                while combat_type != 'cm' and combat_type != 'CM' and combat_type != 'hm' and combat_type != 'HM':
+                    combat_type = input('Sorry, no valid option typed, try again!'
+                                        '\nChaos Magic: [cm/CM]'
+                                        '\nHarmonic Magic: [hm/HM]')
+                    if combat_type == 'cm' or combat_type == 'CM':
+                        damage = character.chaos_magic()
+                        monster.life -= damage
+                        print(f'===== Monster ====='
+                        f'\nSkill damage: {damage}'
+                        f'\nMonster HP: {monster.life}/{monster.max_life}'
+                        f'\n===================')
+                    elif combat_type == 'hm' or combat_type == 'HM':
+                        heal = character.harmonic_magic()
+                        character.life += heal
+                        if character.life > character.max_life:
+                            diff = character.life - character.max_life
+                            character.life -= diff
+                        else:
+                            pass
+                if combat_type == 'cm' or combat_type == 'CM':
+                    damage = character.chaos_magic()
+                    monster.life -= damage
+                    print(f'===== Monster ====='
+                          f'\nSkill damage: {damage}'
+                          f'\nMonster HP: {monster.life}/{monster.max_life}'
+                          f'\n===================')
+                elif combat_type == 'hm' or combat_type == 'HM':
+                    heal = character.harmonic_magic()
+                    character.life += heal
+                    if character.life > character.max_life:
+                        diff = character.life - character.max_life
+                        character.life -= diff
+                    else:
+                        pass
+                    print(f'\n===== Player ====='
+                          f'\nHeal: {heal}'
+                          f'\nHP: {character.life}/{character.max_life}')
             elif status == 's' or status == 'S':
                 print(f'===== Monster ====='
                       f'\nHP: {monster.life}'
                       f'\n===== Player ====='
                       f'\nHP: {character.life}')
+            if monster.life > 0:
+                damage = monster.damage
+                character.life -= damage
+                print()
+                print(f'===== Player ====='
+                      f'\nMonster damage: {damage}'
+                      f'\n{character.name} HP: {character.life}/{character.max_life}'
+                      f'\n==================')
+            else:
+                print('The monster is now dead...')
+                character.xp -= monster.xp
+                if character.xp <= 0:
+                    character.level_up()
             print()
--- jogo_rpg_v02/jobs.py
+++ jogo_rpg_v02/jobs.py
@@ -10,6 +10,10 @@ def job_choice():
         character = warrior(name)
         print('You are now a Warrior!')
         return character
+    elif character == 'm' or character == 'M':
+        character = mage(name)
+        print('You are now a Mage!')
+        return character
     #if character == 'm' or character == 'M':
      #   character = mage(name)
 
@@ -180,20 +184,77 @@ class warrior:
         print(f'Life: {self.life}'
               f'\nLevel: {self.level}')
 
-"""class mage:
+class mage:
     def __init__(self, name):
         self.name = name
-        self.con = 0
-        self.life = self.con * 4
-        self.stre = 0
-        self.intel = 0
-        self.dex = 0
-        self.luck = 0
+        self.con = 2
+        self.life = (self.con * 4)
+        self.max_life = (self.con * 4)
+        self.stre = 1
+        self.intel = 5
+        self.dex = 1
+        self.luck = randint(0, 3) + 2
         self.mana = self.intel * 6
         self.level = 0
         self.xp = self.level * 20
         self.job = 'mage'
 
+    def melee_moves(self):
+        self.kick = self.stre * 2  # Chance to miss
+        self.punch = self.stre * 1.5
+        skill_use = input('Choose your movement!'
+                              '\nKick: [k/K]'
+                              '\nPunch: [p/P]')
+        if skill_use == 'k' or skill_use == 'K':
+            chance = randint(0, 100)
+            if chance > 80:
+                print()
+                print('Kick attack has missed!')
+                return 0
+            else:
+                return int(self.kick)
+        elif skill_use == 'p' or skill_use == 'P':
+            return int(self.punch)
+
+    def harmonic_magic(self):
+        self.heal = int(self.intel * 1.2)
+        skill_use = input('Choose your harmonic spell!'
+                          '\nHeal: [h/H]')
+        while skill_use != 'h' and skill_use != 'H':
+            skill_use = input('Sorry, no valid answer typed, try again!'
+                              '\nHeal: [h/H]')
+            if skill_use == 'h' or skill_use == 'H':
+                return int(self.heal)
+        if skill_use == 'h' or skill_use == 'H':
+            return int(self.heal)
+
+    def chaos_magic(self):
+        self.burn = self.intel * 3
+        self.freeze = self.intel * 3
+        skill_use = input('Choose your chaotic spell!'
+                          '\nBurn (-5 mana): [b/B]'
+                          '\nFreeze (´5 mana): [f/F]')
+        while skill_use != 'b' and skill_use != 'B' and skill_use != 'f' and skill_use != 'F':
+            if skill_use == 'b' or skill_use == 'B':
+                return int(self.burn)
+            elif skill_use == 'f' or skill_use == 'F':
+                return int(self.burn)
+            skill_use = input('Sorry, no valid option typed, try again!'
+                              '\nBurn (-5 mana): [b/B]'
+                              '\nFreeze (-5 mana): [f/F]')
+        if skill_use == 'b' or skill_use == 'B':
+            self.mana -= 5
+            return int(self.burn)
+        elif skill_use == 'f' or skill_use == 'F':
+            self.mana -= 5
+            return int(self.burn)
+
+
+    def att_print(self):
+        print(f'===== Player ====='
+              f'\nHP: {self.life}/{self.max_life}'
+              f'\nMana: {self.mana}'
+              f'\nXP: {self.xp}')
     #need to set skill upgrading to mage context
     def level_up(self): #10 points
         self.level += 1
@@ -285,4 +346,42 @@ class warrior:
                 else:
                     print('No more points to distribute!')
                     if self.level >= 10:
-                        self.skill_upgrade_mage() #doesn't exists yet"""
+                        self.skill_upgrade_mage()
+
+    def skill_upgrade_mage(self):
+        skill_choice = input('Congratulations! You are now lvl 10!'
+                             '\nIts time to choose a skill to upgrade:'
+                             '\nKick: [k/K]'
+                             '\nPunch: [p/P]'
+                             '\nBurn: [b/B]'
+                             '\nFreeze: [f/F]'
+                             '\nHeal: [h/H]')
+        if skill_choice == 'k' or skill_choice == 'K':
+            modifier_choice = input('You have upgraded your Kick skill!'
+                  '\nNow into your skill can be attached a modifier!'
+                  '\n'
+                  '\nChoose a modifier for the skill: '
+                                   '\nCritical Chance (20%): [cc/CC]'
+                                   '\nDamage Increase (+40%): [di/DI]')
+            if modifier_choice == 'cc' or modifier_choice == 'CC':
+                print('Now Kick attack has 20% critical chance (3x damage)!')
+                critical_chance = randint(0, 100)
+                if critical_chance <= 20:
+                    self.kick = (self.stre * 2) * 3
+            elif modifier_choice == 'di' or modifier_choice == 'DI':
+                print('Now Kick attack has +40% damage!')
+                self.kick = (self.stre * 2) + (self.stre * 0.4)
+        if skill_choice == 'p' or skill_choice == 'P':
+            modifier_choice = input('You have upgraded your Punch skill!'
+                                    '\nNow into your skill can be attached a modifier!'
+                                    '\n'
+                                    '\nChoose a modifier for the skill: '
+                                    '\nDouble Punch chance! (30%): [dp/DP]'
+                                    '\nDamage + Dodge chance! (+20%): [dd/DD]')
+            if modifier_choice == 'dp' or modifier_choice == 'DP':
+                print('Now your Punch attack has 30% of double damage!')
+                double_damage_chance = randint(0, 100)
+                if double_damage_chance <= 30:
+                    self.punch = (self.stre * 1.5) * 2
+                else:
+                    self.punch = self.stre * 1.5
